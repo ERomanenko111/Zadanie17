bd9dd66dc01d036ae76d4f4829455883a6e55c6f
ERomanenko111 <romanovc2001@yandex.ru>
2024-07-23 01:28:37 +0300

    first1

--- restdemo/src/main/java/com/example/restdemo/Service/PersonService.java
+++ restdemo/src/main/java/com/example/restdemo/Service/PersonService.java
@@ -3,6 +3,7 @@ package com.example.restdemo.Service;
 import com.example.restdemo.dto.Message;
 import com.example.restdemo.dto.Person;
 import com.example.restdemo.repository.PersonRepository;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +14,12 @@ public class PersonService {
     @Autowired
     PersonRepository repository;
 
-    public Person addMeesageToPerson(int personId, Message message) {
+    public Person addMeesageToPerson(int personId, @NotNull Message message) {
         Person person = repository.findById(personId).get();
         message.setPerson(person);
         message.setTime(LocalDateTime.now());
         person.addMessage(message);
         return repository.save(person);
     }
+
 }
